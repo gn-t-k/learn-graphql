d538f4c62ec650ead3e78f20784e4714136a92f0
gn-t-k <gntk1995422@gmail.com>
2022-09-19 08:50:56 +0900

    wip

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from "apollo-server";
+import { GraphQLScalarType, GraphQLScalarTypeConfig, Kind } from "graphql";
 
 const typeDefs = `
   enum PhotoCategory {
@@ -9,6 +10,7 @@ const typeDefs = `
     GRAPHIC
   }
 
+  scalar DateTime
   type Photo {
     id: ID!
     url: String!
@@ -17,6 +19,7 @@ const typeDefs = `
     category: PhotoCategory!
     postedBy: User!
     taggedUsers: [User!]!
+    created: DateTime!
   }
 
   type User {
@@ -35,7 +38,7 @@ const typeDefs = `
 
   type Query {
     totalPhotos: Int!
-    allPhotos: [Photo!]!
+    allPhotos(after: DateTime): [Photo!]!
   }
 
   type Mutation {
@@ -52,6 +55,7 @@ type Photo = {
   url: string;
   description?: string;
   githubUser: string;
+  created: Date;
 };
 const photos: Photo[] = [
   {
@@ -61,6 +65,7 @@ const photos: Photo[] = [
     url: "https://via.placeholder.com/640x360/0bd/fff?text=1",
     category: "ACTION",
     githubUser: "gPlake",
+    created: new Date("3-28-1977"),
   },
   {
     id: 2,
@@ -68,6 +73,7 @@ const photos: Photo[] = [
     url: "https://via.placeholder.com/640x360/0bd/fff?text=1",
     category: "SELFIE",
     githubUser: "sSchmidt",
+    created: new Date("1-2-1985"),
   },
   {
     id: 3,
@@ -76,6 +82,7 @@ const photos: Photo[] = [
     url: "https://via.placeholder.com/640x360/0bd/fff?text=1",
     category: "LANDSCAPE",
     githubUser: "Schmidt",
+    created: new Date("2018-04-15T19:09:57.308Z"),
   },
 ];
 
@@ -100,10 +107,58 @@ const tags: Tag[] = [
   { photoID: 2, userID: "gPlake" },
 ];
 
+const dateTimeConfig: GraphQLScalarTypeConfig<Date, string> = {
+  name: `DateTime`,
+  description: `A valid date time value.`,
+  parseValue:
+    // クエリの引数から変数で受け取った値をresolverに渡す前の変換
+    (value) => {
+      const isValid = typeof value === "string" || typeof value === "number";
+
+      if (!isValid) {
+        throw new Error("invalid type variable from client");
+      }
+
+      return new Date(value);
+    },
+  serialize:
+    // resolverからクライアントに値を返すときの変換
+    (value) => {
+      const isValid = value instanceof Date;
+
+      if (!isValid) {
+        throw new Error("invalid type value from resolver");
+      }
+
+      return value.toISOString();
+    },
+  parseLiteral:
+    // クエリの引数からリテラルで受け取った値をastから拾い、resolverに渡す前の変換
+    (ast) => {
+      const isValid = ast.kind === Kind.STRING || ast.kind === Kind.INT;
+
+      if (!isValid) {
+        throw new Error("invalid type variable from client");
+      }
+
+      return new Date(ast.value);
+    },
+};
+
 const resolvers = {
   Query: {
     totalPhotos: (): number => photos.length,
-    allPhotos: (): Photo[] => photos,
+    allPhotos: (_parent: any, args: any): Photo[] => {
+      if (!args.after) {
+        return photos;
+      }
+
+      const after: Date = args.after;
+
+      return photos.filter(
+        (photo) => photo.created.getTime() > after.getTime()
+      );
+    },
   },
   Mutation: {
     postPhoto: (
@@ -113,6 +168,7 @@ const resolvers = {
       const newPhoto: Omit<Photo, "url" | "githubUser"> = {
         id: _id++,
         ...args.input,
+        created: new Date(),
       };
       photos.push(newPhoto as Photo);
 
@@ -155,9 +211,11 @@ const resolvers = {
               url: "",
               description: "",
               githubUser: "",
+              created: new Date(),
             }
         ),
   },
+  DateTime: new GraphQLScalarType(dateTimeConfig),
 };
 
 const server = new ApolloServer({
